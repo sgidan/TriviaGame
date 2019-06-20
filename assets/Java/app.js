b16c306335bc39c37629951b6efccf51128ff1a3
sgidan <sarahgidan@gmail.com>
2019-06-20 14:37:30 -0700

    radio buttons working & answers indexed

--- assets/Java/app.js
+++ assets/Java/app.js
@@ -1,82 +1,83 @@
 
 
+
+// $("#startbtn").on("click", start);
+
+// // $(setTimeout(timeUp, 1000 * 30)
+
+
+
+// function start() {
+
+
+
+//     function timeUp() {
+
+//         $("#time-left").append("<h2>Time's Up!</h2>");
+//         console.log("time is up");
+//     }
+//     }
 var questionsArr = [
     {
         Q: "How many colours are there in a rainbow?",
         A: ["7", "5", "8"],
-        correctAns: "7",
+        correctAns: 0,
     },
     {
         Q: "What do you call a time span of one thousand years?",
         A: ["Decade", "Millennium", "Year"],
-        correctAns: "Millennium",
+        correctAns: 1,
     },
     {
         Q: "How many degrees are found in a circle?",
         A: ["30", "60", "90"],
-        correctAns: "30",
+        correctAns: 0,
     },
     {
         Q: "The Dewey Decimal system is used to categorise what?",
         A: ["Books", "Cars", "Websites"],
-        correctAns: "Books",
+        correctAns: 0,
     },
 ]
 
-questionsArr.forEach(function (question) {
+questionsArr.forEach(function (question, questionsIndex) {
 
     var col = $('<div class="col-6">');
     var card = $('<div class="card">');
     var header = $('<div class="card-header">').text(question.Q);
-
-
-
+    var name = questionsIndex;
     col.append(card);
     card.append(header);
 
-    question.A.forEach(function (answers) {
+    question.A.forEach(function (answers, answersIndex) {
         var span = $('<span>');
 
-        var radioBtn = $('<input type="radio" name ="rbtnCount"/>');
-        span.append(radioBtn,answers);
+        var radioBtn = $('<input  class="radio" type="radio"  name="' + questionsIndex + '"index="' + answersIndex + '"/>');
+        span.append(radioBtn, answers);
 
         card.append(span);
 
         console.log(answers);
+
     });
 
     $('.row').append(col);
 
-
-
-
-    // console.log(question.Q);
-    // console.log(question.A);
-    // console.log(question.correctAns);
 });
 
+$('.radio').on('click', function () {
+    var questionsIndex = $(this).attr('name');
+    var answersIndex =$(this).attr('index');
 
+    console.log(questionsIndex, answersIndex);
+    
 
-// for (let i = 0; i < questionsArr.length; i++) {
-//     console.log[i]
-//     console.log(questionsArr[i].A)
+    
+});
 
-//     let currentAns = questionsArr[i].A;
-//     for (let j = 0; j < currentAns.length; j++){
-//         console.log(currentAns[j]);
-//     }
-// }
-
-// questionsArr.forEach(function(element) {
-
-// })
+var userScore;
 
 
 
-//index[i].A[]
-//store answers with index value
-var time = 20
 
-function startGame() {
 
-}
